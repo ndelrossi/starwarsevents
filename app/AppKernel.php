11a94c4921d0187fd37ae3f7c7679c03cba0ab52
Nick DelRossi <nickwtf@gmail.com>
2015-05-28 09:38:33 -0400

    Create login form

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new Yoda\EventBundle\EventBundle(),
+            new Yoda\UserBundle\UserBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
         );
 
